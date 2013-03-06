f7856658f5133bf4135f614765afc67b5d843869
raven99 <shai@shevo.co.il>
2013-03-06 11:04:28 +0200

    Add the interpreter image

--- src/main/java/org/sa/singleton/eager/EagerSingleton.java
+++ src/main/java/org/sa/singleton/eager/EagerSingleton.java
@@ -2,10 +2,17 @@ package org.sa.singleton.eager;
 
 public class EagerSingleton 
 {
-
+	
+	private static EagerSingleton ms_instance = new EagerSingleton();
+	
 	private EagerSingleton()
 	{
 		
+	}
+	
+	public static EagerSingleton getInstance()
+	{
+		return ms_instance;
 	}
 	
 }
