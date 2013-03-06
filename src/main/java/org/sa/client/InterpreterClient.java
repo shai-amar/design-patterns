1a1eb4a657545454b1c8723b42c97a17334d3fa0
raven99 <shai@shevo.co.il>
2013-03-06 12:39:03 +0200

    update the class diagram of the interpreter and the composite

---
+++ src/main/java/org/sa/client/InterpreterClient.java
@@ -0,0 +1,15 @@
+package org.sa.client;
+
+/**
+ * This is the client that demonstrate the use in the Interpreter design pattern.
+ * 
+ * @author Shai Amar
+ *
+ */
+public class InterpreterClient 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
